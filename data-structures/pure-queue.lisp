7088434828551e0bf39d84a373a7eb550ee11fc3
mariari <nymphet@protonmail.com>
2017-10-23 20:53:01 -0400

    fixed my lazy-cons by changing the cdr, thus properly memoizing the data

--- data-structures/pure-queue.lisp
+++ data-structures/pure-queue.lisp
@@ -9,12 +9,11 @@
   (make-queue :q (cons val (queue-q queue)) :d (queue-d queue)))
 
 ;; return a nil value if the queue is empty because that's the CL way, any other language throw an error
-(defun dequeue (queue)
-  (match queue
-    ((Queue :q '()    :d '())          (list nil queue))
-    ((Queue :q queued :d '())          (let ((rev (reverse queued)))
-                                         (list (car rev) (make-queue :d (cdr rev)))))
-    ((Queue :q queued :d (list* x xs)) (list x (make-queue :q queued :d xs)))))
+(defun-match dequeue (queue)
+  ((Queue :q '()    :d '())          (list nil queue))
+  ((Queue :q queued :d '())          (let-match1 (cons x xs) (reverse queued)
+                                       (list x (make-queue :d xs))))
+  ((Queue :q queued :d (list* x xs)) (list x (make-queue :q queued :d xs))))
 
 (defun empty-p (queue)
   (equalp (make-queue) queue))
@@ -28,3 +27,10 @@
 (defparameter *queue-test* (dequeue
                             (enqueue-seq '(3 34 4 5 6 7 8)
                                          (cadr (dequeue (enqueue-many (make-queue) 2 3 4 5 6))))))
+
+;;; A queue with 3 lists***********************************************************************************************
+
+;; we will first have to implement lazyness in CL.. IΕ after I read more of okasaki's book
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defstruct t-queue (q '() :type list) (d '() :type list) (d '() :type list)))
--- lazy-cons.lisp
+++ lazy-cons.lisp
@@ -1,40 +1,61 @@
 ;; I don't have the environment, so I'll just use lambda to delay evaluation!
 ;; lambda the ultimate lazy
 
-
 (defmacro delay (n) `(lambda () ,n))
 
 (defun force (n) (funcall n))
 
+
+(defmacro scons (x xs)
+  `(cons ,x (delay ,xs)))
+
+(defun sterms (n)
+  (scons (/ 1 (expt n 2))
+         (sterms (1+ n))))
+
+(defun scar (lis) (car lis))
+
+(defun scdr (lis)
+  (if (functionp (cdr lis))
+      (setf (cdr lis) (force (cdr lis)))
+      (cdr lis)))
+
+
+(defparameter *test2* (cons-stream 1 (cons-stream 2 nil)))
+
+(scdr (scdr *test2*))
+
+(defparameter *sterms* (sterms 1))
+
+;; time to add a match pattern to this
+
+
+
+
+;; First attempt at lazyness********************************************************************************************
 (defun terms (n)
   (cons (/ 1 (expt n 2))
         (lambda () (terms (1+ n)))))
 
-(defun scons (x xs)
-  (cons (delay x)
-        (delay xs)))
+;; A better way to define it!,
+(defun scons% (x xs)
+  (cons (delay x) (delay xs)))
 
-
-(defun sterms (n)
+(defun sterms% (n)
   (scons (/ 1 (expt n 2))
-         (terms (1+ n))))
+         (sterms% (1+ n))))
+
+(defun scar% (lis) (force (car lis)))
+
+(defun scdr% (lis) (force (cdr lis)))
 
 
-(defun scar (lis) (force (car lis)))
+;;; Second Attempt at lazyness that failed******************************************************************************
 
-(defun scdr (lis) (force (cdr lis)))
-
-
-;;  this model is inferior as we would have to recompute the values
-;; lets try again
-
-(defmacro scons% (x xs)
+(defmacro scons%% (x xs)
   `(cons ',x ,xs))
 
 
-(scons% (+ 3 4) (scons% (+ 1 3) nil))
+(defun scar%% (n) (eval (car n)))
 
-(defun scar% (n) (eval (car n)))
-
-
-;;  failure again lets go back to scar and scdr
+;; (scons% (+ 3 4) (scons% (+ 1 3) nil))
