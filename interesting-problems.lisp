fa8c8bcd53c3f31d28bf4808aeed4232a4f1aef0
mariari <nymphet@protonmail.com>
2017-12-11 02:57:43 -0500

    updated the solution a bit

--- interesting-problems.lisp
+++ interesting-problems.lisp
@@ -4,31 +4,32 @@
 ;; this structure can hold. Guy Steele likes to talk about this problem a fair
 ;; Here is my solution
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (ql:quickload 'trivia)
+  (ql:quickload '(:fare-quasiquote-readtable
+                  :trivia.quasiquote
+                  :trivia))
   (use-package 'trivia))
+(named-readtables:in-readtable :fare-quasiquote)
+
 
 (defun water-sustained (xs)
   (let* ((max-l (max-left xs))
          (max-r (max-right max-l :key #'car)))
     (reduce (lambda (acc x)
               (match x
-                ((list (list val max-left) max-right)
-                 (let ((min (min max-left max-right)))
-                   (+ acc
-                      (if (>= min val) (- min val) 0))))))
+                (`((,val ,max-left) ,max-right)
+                  (+ acc (max (- (min max-left max-right) val) 0)))))
             max-r :initial-value 0)))
-
 
 (defun max-gen (xs &key (key #'identity))
   (reduce (lambda (ys x)
             (match ys
-              ((list* (list val max) _)
+              ((cons `(,val ,max) _)
                (cons (list x (if (> max (funcall key val))
-                                 max (funcall key val)))
+                                 max
+                                 (funcall key val)))
                      ys))))
           (cdr xs)
-          :initial-value (list (list (car xs)
-                                     (funcall key (car xs))))))
+          :initial-value `((,(car xs) ,(funcall key (car xs))))))
 
 (defun max-left  (xs &key (key #'identity)) (reverse (max-gen xs :key key)))
 (defun max-right (xs &key (key #'identity)) (max-gen (reverse xs) :key key))
