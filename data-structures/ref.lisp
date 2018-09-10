5ffba33fc82fd92e287bfadb93e23ce5996ae08d
mariari <nymphet@protonmail.com>
2018-09-09 21:22:54 -0400

    fixed documentation to references

--- data-structures/ref.lisp
+++ data-structures/ref.lisp
@@ -1,14 +1,17 @@
 (defstruct ref contents)
 
 (defun ref (x)
+  "Creates a reference out of x"
   (make-ref :contents x))
 
 (defun ! (ref)
+  "Grabs the contents of a reference"
   (ref-contents ref))
 
 (defun := (ref x)
-  "sets the "
+  "sets the reference value to x"
   (setf (ref-contents ref) x))
 
 (defun (setf !) (x ref)
+  "sets the reference value to x"
   (:= ref x))
