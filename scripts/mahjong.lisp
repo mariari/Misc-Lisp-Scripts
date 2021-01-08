899cf6ec69e0a07cd381a824f5f3a57c4835ecb8
mariari <mariari@protonmail.com>
2021-01-07 23:56:26 -0600

    added a tsumo win and ron win point calculator

--- scripts/mahjong.lisp
+++ scripts/mahjong.lisp
@@ -3,9 +3,31 @@
   (:use #:cl)
   (:export
    :calculate-score
-   :round-up-ten))
+   :round-up-ten
+   :total-tsumo-score
+   :ron-win))
 
 (in-package :scripts.mahjong)
+
+(defun ron-win (&key han (fu 20) eastp)
+  (let ((score (calculate-score :han han :fu fu)))
+    (if eastp
+        (* 6 score)
+        (* 4 score))))
+
+(defun total-tsumo-score (&key han (fu 20) eastp)
+  (let ((payout (tsumo-win :han han :fu fu :eastp eastp)))
+    (trivia:match payout
+      ((list :others score) (* 3 score))
+      ((list :others other :east east) (+ east (* 2 other))))))
+
+(defun tsumo-win (&key han (fu 20) eastp)
+  (let* ((score          (calculate-score :han han :fu fu))
+         (rounded        (round-up-to 100 score))
+         (rounded-double (round-up-to 100 (* 2 score))))
+    (if eastp
+        (list :others rounded-double)
+        (list :others rounded :east rounded-double))))
 
 (defun calculate-score (&key han (fu 20))
   (cond
@@ -14,10 +36,13 @@
     ((>= han 8)  4000)
     ((>= han 6)  3000)
     ((>= han 5)  2000)
-    (t           (min 2000 (* (round-up-ten fu) (expt 2 (+ 2 han)))))))
+    (t           (min 2000 (* (round-up-to 10 fu) (expt 2 (+ 2 han)))))))
 
-(defun round-up-ten (fu)
-  (let ((moded (mod fu 10)))
+(defun round-up-10 (fu)
+  (round-up-to 10 fu))
+
+(defun round-up-to (base fu)
+  (let ((moded (mod fu base)))
     (if (zerop moded)
         fu
-        (+ fu (- 10 (mod fu 10))))))
+        (+ fu (- base (mod fu base))))))
