0e2657ea47a981fab35d681082d991647e84b94b
mariari <mariari@protonmail.com>
2021-01-18 22:12:56 -0600

    added functionality so it doesn't repeat itself in transcoding

--- scripts/akochan.lisp
+++ scripts/akochan.lisp
@@ -2,7 +2,8 @@
 (defpackage #:scripts.akochan
   (:use #:uiop #:inferior-shell)
   (:use #:common-lisp)
-  (:export :akochan))
+  (:export :akochan
+           :make-html-name))
 
 (in-package #:scripts.akochan)
 
@@ -23,6 +24,13 @@
 ;; the system executable, they should also be in the akochan directory
 ;; as well
 
+(defun make-html-name (file)
+  (concatenate 'string
+               (directory-namestring file)
+               "analysis/"
+               (pathname-name file)
+               ".html"))
+
 (defun akochan (name dir &rest body)
   (let ((files (uiop:directory-files dir)))
     (mapc
@@ -33,26 +41,31 @@
                 (cl-json:decode-json-from-string file-string))
               (pos
                 (position name (cdr (assoc :name decoded))
-                          :test #'string=)))
-         (run `(bash -c
-                     ,(concatenate
-                       'string
-                       ;; we need lib-ai on this path
-                       ;; for some reason the linker doesn't work
-                       "LD_LIBRARY_PATH=~/.local/lib/akochan \\
+                          :test #'string=))
+              (output-name
+                (make-html-name file)))
+         ;; check if the file is already fixed
+         ;; if so, then skip it
+         (unless (probe-file output-name)
+           (run `(bash -c
+                       ,(concatenate
+                         'string
+                         ;; we need lib-ai on this path
+                         ;; for some reason the linker doesn't work
+                         "LD_LIBRARY_PATH=~/.local/lib/akochan \\
                        akochan-reviewer \\
                        --lang en \\
                        -d ~/.local/lib/akochan/akochan/ \\
                        -c ~/.local/lib/akochan/tactics.json \\
                        -no-open "
-                       "-a "
-                       (format nil "~{~A~^ ~}" body)
-                       (or (write-to-string pos) 0)
-                       " -i "
-                       (namestring file)
-                       " -o "
-                       (concatenate 'string
-                                    (directory-namestring file)
-                                    "analysis/"
-                                    (file-namestring file)))))))
+                         "-a "
+                         (format nil "~{~A~^ ~}" body)
+                         (or (write-to-string pos) 0)
+                         " -i "
+                         (namestring file)
+                         " -o "
+                         (concatenate 'string
+                                      (directory-namestring file)
+                                      "analysis/"
+                                      (file-namestring file))))))))
      files)))
