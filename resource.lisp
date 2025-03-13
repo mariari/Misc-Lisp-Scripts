69c2e90ef0582284f0fdd570d898045ce4ce1e55
mariari <mariari@protonmail.ch>
2025-03-14 01:45:42 +0800

    RMV2: compile methods to resources

--- resource.lisp
+++ resource.lisp
@@ -39,15 +39,24 @@
 ;; I can form the rest as well though the exact machinery around
 ;; forming actions is more annoying to do properly
 
-;; Let us model the prove and verify arguments
-;; We can model some of it
+;; Our instance is a rough approximation of a proving context for an
+;; individual resource... This not completely accurate
 (defclass instance ()
   ((tag :initarg :tag :accessor tag)
    (consumed-p :initarg :consumed-p :accessor consumed-p :type boolean :initform t)
    (consumed   :initarg :consumed :accessor consumed :type list :initform nil)
-   (created    :initarg :created  :accessor created  :type list :initform nil)
-   (app-data   :initarg :app-data :accessor app-data)))
+   (created    :initarg :created  :accessor created  :type list :initform nil)))
 
+
+(defclass compliance-unit ()
+  ((proof    :initarg :proof :accessor proof :initform nil)
+   (vk       :initarg :verifying-key :accessor verifying-key :initform nil)
+   (instances :initarg :instances :accessor instances :initform nil))
+  (:documentation "In a transparent case the only real field is instance"))
+
+(-> make-compliance-unit (list) compliance-unit)
+(defun make-compliance-unit (instances)
+  (values (make-instance 'compliance-unit :instances instances)))
 
 ;; Instead let us model our own view of objects into a resource Let us
 ;; split the following, we have some methods that compile to an
@@ -116,7 +125,6 @@
   ;; We can ignore the instance as our constraint is very simple
   (integerp (data object)))
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Methods as resources
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -135,7 +143,72 @@
 ;; value?
 
 
+(defclass method-resource ()
+  ;; type is funcallable to be more accurate.
+  ;; If we have more outputs, then we'd care about that as a field
+  ((gf :initarg :gf :accessor gf :type symbol)))
 
+(defmethod obj-resource-logic ((object method-resource) (instance instance) any)
+  (obj-equalp
+   ;; We check the output is equal to the work
+   (resource->obj (cadr (created instance)))
+   (apply (gf object)
+          (mapcar #'resource->obj (consumed instance)))))
+
+(defun add (arg1 &rest argn)
+  (reduce #'add-2 argn :initial-value arg1))
+
+;; Let us make our first operation
+(defmethod add-2 ((int1 integer-obj)
+                  (int2 integer-obj))
+  ;; if something is a subclass make that instead
+  (make-instance (class-of int1)
+                 :data (+ (data int1)
+                          (data int2))))
+
+;; RMV2> (add (counted 6) (counted 5) (mk-integer 12))
+
+;; We can verify this in an interesting way
+;; 1. eval then package it into a resource
+;; 2. leave it open to solve as an intent bearing transaction
+
+(defmacro transact (expression)
+  `(transact-expression (list ',(car expression)
+                              ,@(cdr expression))
+                        ,expression))
+
+(defun transact-expression (expression result)
+  (let ((consumed (mapcar #'obj->resource (cdr expression)))
+        (output   (obj->resource result))
+        ;; just using the car isn't the most elegant
+        (function (obj->resource
+                   (make-instance 'method-resource :gf (car expression)))))
+    (labels ((create-consumed (object)
+               (make-instance 'instance
+                              :created (list function output)
+                              :consumed consumed
+                              :consumed-p t
+                              ;; modeling of tag not online
+                              :tag object))
+             (create-output (object)
+               (let ((obj (create-consumed object)))
+                 (setf (consumed-p obj) nil)
+                 obj)))
+      (make-compliance-unit
+       ;; Order is: function, output, inputs
+       (list* (create-output function)
+              (create-output output)
+              (mapcar #'create-consumed consumed))))))
+
+;; Now let us verify our compliance unit
+(-> verify-compliance-unit (compliance-unit) boolean)
+(defun verify-compliance-unit (compliance)
+  (let ((instances (instances compliance)))
+    (every (lambda (instance)
+             (verify (tag instance)
+                     instance
+                     t))
+           instances)))
 
 ;; (defvar *known-verification-functions* (make-hash-table))
 
@@ -151,6 +224,27 @@
 ;; ("hi")
 ;; RMV2> (verify *x* (make-instance 'instance) 3)
 ;; NIL
+
+;; RMV2>
+;; (verify-compliance-unit
+;;  (transact (add (counted 6)
+;;                 (add (counted 20) (counted 122))
+;;                 (counted 5)
+;;                 (mk-integer 12))))
+;; T
+
+;; RMV2>
+;; (defparameter *transaction* (transact (add (counted 6)
+;;                                            (add (counted 20) (counted 122))
+;;                                            (counted 5)
+;;                                            (mk-integer 12))))
+;; *TRANSACTION*
+;; RMV2> (defparameter *add* (car (instances *transaction*)))
+;; *ADD*
+
+;; RMV2> (setf (consumed *add*) (cons (car (consumed *add*)) (consumed *add*)))
+
+;; RMV2> (verify-compliance-unit *transaction*)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Generalized helpers
@@ -170,6 +264,8 @@
          (format stream "~2I~{~_~A~^ ~}" (to-list obj))))))
 
 (define-generic-print resource)
+(define-generic-print instance)
+(define-generic-print compliance-unit)
 (define-generic-print integer-obj)
 (define-generic-print counted-integer)
 
@@ -197,7 +293,8 @@
   (:documentation "Gets all the instance values of a list")
   (:method ((object standard-object))
     (mapcar (lambda (x) (slot-value object x))
-            (mapcar #'c2mop:slot-definition-name (pointwise-slots object)))))
+            (mapcar #'c2mop:slot-definition-name
+                    (instance-slots (class-of object))))))
 
 (defgeneric class-values (object)
   (:documentation "Gets all the class values of a list")
